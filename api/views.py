75918295e9d42c100806fb37d2621799e5c96b23
Kevin Abraham <myservices@kabes.org>
2024-01-20 15:04:54 -0500

    added ability to retrieve restaurant associated with user

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from .models import Restaurant, Ticket
 from .serializers import RestaurantSerializer, TicketSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class RestaurantViewSet(viewsets.ModelViewSet):
@@ -12,3 +13,12 @@ class RestaurantViewSet(viewsets.ModelViewSet):
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+
+
+class UserRestaurantsViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = RestaurantSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Restaurant.objects.filter(user=user)
--- food_saver/urls.py
+++ food_saver/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
-from api.views import RestaurantViewSet, TicketViewSet
+from rest_framework.routers import DefaultRouter
+from api.views import RestaurantViewSet, TicketViewSet, UserRestaurantsViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
-router = SimpleRouter()
+
+router = DefaultRouter()
 
 router.register(r'restaurants', RestaurantViewSet)
 router.register(r'tickets', TicketViewSet)
-
+router.register(r'user-restaurants', UserRestaurantsViewSet, basename='user-restaurants')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/login/', obtain_auth_token), 
 ]
