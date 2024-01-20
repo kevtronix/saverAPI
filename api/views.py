c9baefc94fbd1fffb1b42334bc49fa5983e1fae3
Kevin Abraham <myservices@kabes.org>
2024-01-20 18:20:06 -0500

    Allow only single super user to be able to see all of the different tickets that were created

--- api/views.py
+++ api/views.py
@@ -3,7 +3,11 @@ from rest_framework.views import APIView
 from .models import Restaurant, Ticket
 from .serializers import RestaurantSerializer, TicketSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 
+class IsSuperUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_superuser
 
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
@@ -13,6 +17,7 @@ class RestaurantViewSet(viewsets.ModelViewSet):
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated, IsSuperUser]
 
 
 class UserRestaurantsViewSet(viewsets.ReadOnlyModelViewSet):
